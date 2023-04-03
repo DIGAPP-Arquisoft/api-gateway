be96a6119120471f64f15b9dff4970f598b9ab4d
David Rios <drriosg@unal.edu.co>
2023-04-03 17:26:39 -0500

    Inicio API Gateway ApolloClient

---
+++ index.js
@@ -0,0 +1,38 @@
+import {ApolloServer, gql} from 'apollo-server';
+import axios from 'axios';
+
+const {data: users} = await axios.get('http://localhost:3000/user')
+const {data: establishments} = await axios.get('http://localhost:3000/user')
+const {data: users} = await axios.get('http://localhost:3000/user')
+const {data: users} = await axios.get('http://localhost:3000/user')
+
+
+const typeDefs = gql`
+    type User {
+        UserID: ID!
+        UserName: String!
+        Password: String!
+        Email: String!
+        UserPhoto: String!
+        City: Int!
+    }
+
+    type Query {
+        usersCount: Int!
+        allUsers: [User]!
+    }
+`
+
+const resolvers = {
+    Query: {
+        usersCount: () => users.length,
+        allUsers: () => users
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+server.listen().then(({url}) => console.log(`Server ready at ${url}`))
