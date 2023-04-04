4fb72b3ad44f0a88aef6647579615a3bb10443a7
David Rios <drriosg@unal.edu.co>
2023-04-03 23:18:18 -0500

    Avances en las Querys

--- index.js
+++ index.js
@@ -1,36 +1,89 @@
-import {ApolloServer, gql} from 'apollo-server';
-import axios from 'axios';
+import { ApolloServer, gql } from "apollo-server";
+import axios from "axios";
 
-const {data: users} = await axios.get('http://localhost:3000/user')
-//const {data: establishment} = await axios.get('http://localhost:3000/user')
-
+const users = await axios.get("http://localhost:3000/user").then(res => {return res.data});
+const establishments = await axios.get(
+    "http://localhost:3000/establishment"
+    ).then(res=>{return res.data});
+const reports = await axios.get("http://localhost:3000/report").then(res=>{return res.data});
 
 const typeDefs = gql`
-    type User {
-        UserID: ID!
-        UserName: String!
-        Password: String!
-        Email: String!
-        UserPhoto: String!
-        City: Int!
-    }
+  type User {
+    UserID: ID!
+    UserName: String!
+    Password: String!
+    Email: String!
+    UserPhoto: String!
+    City: Int!
+  }
 
-    type Query {
-        usersCount: Int!
-        allUsers: [User]!
-    }
-`
+  type Establishment {
+    EstablishmentID: ID!
+    UserID: Int!
+    EstablishmentName: String!
+    Opening: String!
+    Closing: String!
+    EstablishmentType: String!
+    Capacity: Int!
+    InternetQuality: Int!
+    Rating: String!
+    Description: String!
+    Location: String!
+    City: Int!
+  }
+
+  type Report {
+    ReportID: ID!
+    UserID: Int!
+    EstablishmentID: Int!
+    Date: String!
+    InternetQuality: String!
+    CapabilityID: String!
+    ScoreEstablishment: String!
+    ScoreReport: String!
+    Review: String!
+   }
+
+  type Query {
+    # Users
+    allUsers: [User]!
+
+    # Establishments
+    allEstablishtmets: [Establishment]!
+    findEstablishment(EstablishmentID: Int!): Establishment
+
+    # Reports
+    allReports: [Report]!
+    findReports(EstablishmentID: Int!): [Report]
+  }
+`;
 
 const resolvers = {
-    Query: {
-        usersCount: () => users.length,
-        allUsers: () => users
+  Query: {
+
+    //Users
+    allUsers: () => users,
+
+    //Establishments
+    allEstablishtmets: () => establishments,
+    findEstablishment: (root, args) => {
+        const {EstablishmentID} = args
+        return establishments.find(est => est.EstablishmentID === EstablishmentID)
+    },
+
+
+    //Reports
+    allReports: () => reports,
+    findReports: (root, args) => {
+        const {EstablishmentID} = args
+        return reports.filter(rep => rep.EstablishmentID === EstablishmentID)
     }
-}
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-})
+  typeDefs,
+  resolvers,
+});
 
-server.listen().then(({url})=> console.log(`server ready at ${url}`))
+server.listen().then(({ url }) => console.log(`server ready at ${url}`));
