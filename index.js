8a64703f75b0c9ea413c03f6a42595914f5bc3c7
David Rios <drriosg@unal.edu.co>
2023-04-07 21:36:50 -0500

    Avances en APIGateway

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 import { ApolloServer, gql } from "apollo-server";
 import axios from "axios";
 
-const users = await axios.get("http://localhost:3000/user").then(res => {return res.data});
-const establishments = await axios.get(
-    "http://localhost:3000/establishment"
-    ).then(res=>{return res.data});
-const reports = await axios.get("http://localhost:3000/report").then(res=>{return res.data});
-const favorites = await axios.get("http://localhost:3000/favorites").then(res=>{return res.data}) 
 
-// cover_picture = models.CharField(max_length=300, null=True, blank=True)
+// Funciones para realizar peticiones a los MS
+function getUsers() { 
+  return axios.get("http://localhost:3000/user").then(res => res.data)
+}
+
+function getEstablishments(){
+  return axios.get("http://localhost:3000/establishment").then(res => res.data)
+}
+
+function getReports(){
+  return axios.get("http://localhost:3000/report").then(res => res.data)
+}
+
+function getFavorites(){
+  return axios.get("http://localhost:3000/favorites").then(res => res.data)
+}
+
+function getBookings(){
+  return axios.get("http://localhost:3000/booking").then(res => res.data)
+}
+
 
 const typeDefs = gql`
+
+  # ------------------------------- #
+  # ------- DIG Definitions ------- #
+  # ------------------------------- #
+
   type User {
-    UserID: ID!
+    id: ID!
     UserName: String!
     Password: String!
     Email: String!
@@ -26,7 +45,9 @@ const typeDefs = gql`
     EstablishmentName: String!
     Opening: String!
     Closing: String!
+    Menu: String!
     EstablishmentType: String!
+    Bookings: Int!
     Capacity: Int!
     InternetQuality: Int!
     Rating: String!
@@ -34,11 +55,12 @@ const typeDefs = gql`
     CoverPicture: String!
     Location: String!
     City: Int!
-    Reports: [Report]!
+    Reports: [Report]
+    Statistics: Statistic
   }
 
   type Report {
-    ReportID: ID!
+    id: ID!
     UserID: Int!
     EstablishmentID: Int!
     Date: String!
@@ -54,10 +76,27 @@ const typeDefs = gql`
     UserID: Int!
   }
 
+  type Statistic {
+    IQAverage: Float!
+    SEAverage: Float!
+  }
+
+  type Bookings {
+    UserID: Int!
+    EstablishmentID: Int!
+    Date: String!
+    Hour: String!
+    NumberofPeople: Int!
+  }
+
+  # ------------------------------- #
+  # ---------- Consultas ---------- #
+  # ------------------------------- #
+
   type Query {
     # Users
     allUsers: [User]!
-    findUser(UserID: Int!): User
+    findUser(id: Int!): User
 
     # Establishments
     allEstablishtmets: [Establishment]!
@@ -68,8 +107,66 @@ const typeDefs = gql`
     findReports(EstablishmentID: Int!): [Report]
 
     # Favorites
-    favoritesByID(UserID: Int!): [Favorite]!
-    findFavorites(UserID: Int!): [Establishment]!
+    favoritesByID(id: Int!): [Favorite]!
+    findFavorites(id: Int!): [Establishment]!
+  }
+  
+  # -------------------------------- #
+  # ------- Adds and Deletes ------- #
+  # -------------------------------- #
+
+  type Mutation {
+
+    addUser(
+      UserName: String!
+      Password: String!
+      Telephone: String!
+      Email: String!
+      UserPhoto: String!
+      City: Int!
+    ): User
+
+    addEstablishment(
+      UserID: Int!
+      EstablishmentName: String!
+      Opening: String!
+      Closing: String!
+      Menu: String!
+      EstablishmentType: String!
+      Capacity: Int!
+      InternetQuality: Int!
+      Rating: String!
+      Description: String!
+      CoverPicture: String!
+      Location: String!
+      City: Int!
+    ): Establishment\
+
+    addReport(
+      UserID: Int!
+      EstablishmentID: Int!
+      Date: String!
+      InternetQuality: Int!
+      CapabilityID: Int!
+      ScoreEstablishment: String!
+      ScoreReport: String!
+      Review: String!
+    ): Report
+
+
+    addFavorite(
+      EstablishmentID: Int!
+      UserID: Int!
+    ): Favorite
+
+    addBooking(
+      UserID: Int!
+      EstablishmentID: Int!
+      Date: String!
+      Hour: String!
+      NumberofPeople: Int!
+    ): Bookings
+    
   }
 `;
 
@@ -77,50 +174,124 @@ const resolvers = {
   Query: {
 
     //Users
-    allUsers: () => users,
-    findUser: (root, args) => {
-        const { UserID } = args
-        return users.find(usr => usr.UserID === UserID)
+    allUsers: async () => {
+      const users = await getUsers()
+      return users
+    },
+    findUser: async (root, args) => {
+        const { id } = args
+        const users = await getUsers()
+        return users.find(usr => usr.id === id)
     },
 
     //Establishments
-    allEstablishtmets: () => establishments,
-    findEstablishment: (root, args) => {
-        const { EstablishmentID }  = args
-        return establishments.find(est => est.EstablishmentID === EstablishmentID)
+    allEstablishtmets: async () => {
+      const establishments = await getEstablishments()
+      return establishments
+    },
+    findEstablishment: async (root, args) => {
+      const establishments = await getEstablishments()
+      const {EstablishmentID}  = args
+      return establishments.find(est => est.EstablishmentID === EstablishmentID)
     },
 
     //Reports
-    allReports: () => reports,
-    findReports: (root, args) => {
+    allReports: async () => {
+      const reports = await getReports()
+      return reports
+    },
+    findReports: async (root, args) => {
+      const reports = await getReports()
         const { EstablishmentID } = args
         return reports.filter(rep => rep.EstablishmentID === EstablishmentID)
     },
 
-    
     //Favorites
-    findFavorites: (root, args) => {
-      const {UserID} = args
-      const favsUrs = favorites.filter(fav => fav.UserID === UserID)
+    findFavorites: async (root, args) => {
+      const favorites = await getFavorites()
+      const establishments = await getEstablishments()
+      const {id} = args
+      const favsUrs = favorites.filter(fav => fav.UserID === id)
       return favsUrs.map(element => {
         const favtemp = establishments.find(est => est.EstablishmentID === element.EstablishmentID)
         return favtemp
       });
-    }
+    },
 
+  },
+
+  Mutation: {
+
+    addUser: (root, args) => {
+      const user = {...args}
+      // To do Post Request from User
+      return user
+    },
+
+    addEstablishment: (root, args) => {
+      const establishment = {...args}
+      // To do Post Request from Establishmet
+      return establishment
+    },
+
+    addReport: (root, args) => {
+      const report = {...args}
+      // To do Post Request from Report
+      return report
+    },
+
+    addFavorite: (root, args) => {
+      const favorite = {...args}
+      // To do Post Request from Favorite
+      return favorite
+    },
+
+    addBooking: (root, args) => {
+      const booking = {...args}
+      // To do Post Request from Booking
+      return booking
+    },
 
   },
 
   Establishment: {
-    Reports: (root) => {
-        const {EstablishmentID} = root
-        return reports.filter(rep => rep.EstablishmentID === EstablishmentID)
+
+    // Find and returns the Reports by EstablishmentID
+    Reports: async (root) => {
+        const reps = await getReports()
+        const { EstablishmentID } = root
+        const reports = reps.filter(rep => rep.EstablishmentID === EstablishmentID)
+        if (reports.length === 0) return null
+        return reports
+    },
+
+    // Find and retunrs the numbers of bookings by EstablishmentID
+    Bookings: async (root) => {
+      const {EstablishmentID} = root
+      const bkgs = await getBookings()
+      const bookings = bkgs.filter(b => b.EstablishmentID === EstablishmentID)
+      if (bookings.length === 0) return null
+      return bookings.reduce((a, b) => a + b.NumberofPeople, 0)
+    },
+
+    // Calculate and returns the aveage of InternetQality and EstablishmentStatus
+    Statistics: async (root) => {
+      const { EstablishmentID } = root
+      const reps = await getReports()
+      const reports = await reps.filter(rep => rep.EstablishmentID === EstablishmentID)
+      if (reports.length === 0) return null
+      const IQAv = reports.reduce((a, b) => a + b.InternetQuality, 0)/ reports.length;
+      const SEAv = reports.reduce((a, b) => a + b.ScoreEstablishment, 0)/ reports.length;
+      return{
+        IQAverage: IQAv,
+        SEAverage: SEAv
+      }
     }
   }
 };
 
 
-
+// Creating of Apollo-Server(Server from Graphql) 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
